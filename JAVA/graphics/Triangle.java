0340a0af34a081fe2caa295f1b0469b26b7665c7
Anjaly gigi <150697743+Anjaly-Gigi@users.noreply.github.com>
2024-07-18 20:19:55 +0530

    Add files via upload

---
+++ JAVA/graphics/Triangle.java
@@ -0,0 +1,16 @@
+package graphics;
+
+public class Triangle
+	{
+	   double length,height;
+	   public Triangle(double length,double height)
+	   	{
+	   	  this.length=length;
+	   	  this.height=height;
+	   	}
+	   public double area()
+	   	{
+	   	  return .5*length*height;
+	   	}
+	   
+	}
